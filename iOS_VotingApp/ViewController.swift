e791fec881ab82e05c05ba255e794569b05c8d20
phai0512 <phai0512@gmail.com>
2018-03-04 13:50:14 +0900

    Initial Commit

---
+++ iOS_VotingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_VotingApp
+//
+//  Created by 小池基文 on 2018/03/04.
+//  Copyright © 2018年 iOS_Training_day3_2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
