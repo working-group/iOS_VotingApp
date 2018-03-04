6370490a461d2f4c78735c2c4f3f20e3d8620c04
phai0512 <phai0512@gmail.com>
2018-03-04 14:27:17 +0900

    create tableview

---
+++ iOS_VotingApp/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  iOS_VotingApp
+//
+//  Created by 小池基文 on 2018/03/04.
+//  Copyright © 2018年 iOS_Training_day3_2. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
